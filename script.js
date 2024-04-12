80d34489fcf1a73766d43196fdbeb59c3d48e620
JewellianaC <144458061+JewellianaC@users.noreply.github.com>
2024-04-12 08:17:02 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const hexagon = document.getElementById('hexagon')
+
+function alertMe(){
+    console.log('clicked')
+}
+
+hexagon.addEventListener('click',alertMe)
+
+//hexagon.addEventListener('mouseover',alertMe)
